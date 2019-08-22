faaa78cf44f44b85e8aa32d9c97ea4831775d624
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2019-08-22 10:34:24 +0530

    Adding bootstrap in this project
    Hi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1 className="text-danger">Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
